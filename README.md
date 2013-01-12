d9e4d8cd881040c786ec5420de75eac106ee02c7
Yosuke Tomita <seikoudoku777@gmail.com>
2013-01-13 02:09:48 +0900

    dummy implementation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-http://d.hatena.ne.jp/t-wada/20100228/p1 の写経です
+http://d.hatena.ne.jp/t-wada/20100228/p1 の写経です。
+
+※実行環境
+ruby1.9.2
+rspec2.12.2
+
+※環境差分メモ：
+spec -> rspec
+require の$LOAD_PATH にカレントディレクトリが含まれないので、相対パスで表す
+http://blog.ruedap.com/entry/20110531/ruby_require_load_path
--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -3,6 +3,14 @@ require 'rspec'
 require './message_filter.rb'
 
 describe MessageFilter do
-	
+	it 'should detect message with NG word' do
+		filter = MessageFilter.new('foo')
+		filter.detect?('hello from foo').should == true
+	end
+
+	it 'should not detect message with NG word' do
+		filter = MessageFilter.new('foo')
+		filte.detect?('hello, world!').should == false
+	end
 end
 
