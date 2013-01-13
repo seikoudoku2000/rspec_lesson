964a38267c28e3eb2f079b4b70ffb7c1462c654e
Yosuke Tomita <seikoudoku777@gmail.com>
2013-01-14 05:04:46 +0900

    refactor test code

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,17 +2,9 @@ require 'rubygems'
 require 'rspec'
 require './message_filter.rb'
 
-describe MessageFilter do
-	before do
-		@filter = MessageFilter.new('foo')
-	end
-
-	it 'should detect message with NG word' do
-		@filter.should be_detect('hello from foo')
-	end
-
-	it 'should not detect message with NG word' do
-		@filter.should_not be_detect('hello, world!')
-	end
+describe MessageFilter, 'with argument "foo"' do
+	subject {MessageFilter.new('foo')}
+	it {should be_detect('hello from foo')}
+	it {should_not be_detect('hello, world!')}
 end
 
